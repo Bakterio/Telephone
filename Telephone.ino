b95de087e314e234b24b4ba637ef25ae4b66484f
bakterio <lukastoman594@gmail.com>
2022-07-26 21:10:08 +0200

    lastTime

--- Telephone.ino
+++ Telephone.ino
@@ -6,6 +6,7 @@ int greenP = 4;
 
 int count = 0;
 int lastTime = millis();
+int lastStatus = LOW;
 
 void setup() {
   Serial.begin(9600);
@@ -21,9 +22,10 @@ void loop() {
  if (debug) Serial.println("Y - " + String(yellow) + " W - " + String(white) + " G - " + String(green));
 
  if (white == LOW) {
-    if (yellow == HIGH) {
+    if (yellow == HIGH && lastStatus == LOW) {
+      //Serial.println("counting");
       int deltaTime = lastTime - millis();
-      Serial.println(deltaTime);
+      if (debug) Serial.println(deltaTime);
       if (deltaTime == 0) return;
       count++;
       lastTime = millis();
@@ -32,16 +34,19 @@ void loop() {
         Serial.println(deltaTime);
       }
     }
+    lastStatus = yellow;
   } else if (count != 0) {
-    count++;
     if (debug) {
-      Serial.println("Suma - " + String(count/2));
+      Serial.println("Suma - " + String(count));
       Serial.print("\n\n\n\n\n");
       delay(1000);
     } else {
-      Serial.println("Suma - " + String(count/2));
+      Serial.print(String(count) + ",");
     }
     count = 0;
  }
- 
+}
+
+void displayNumber(int number) {
+//nemám odpor, takže to nemůžu zkoušet :(
 }
